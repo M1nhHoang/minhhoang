fc76106e3325728afdf10b5d4ea1130c8cb303a9
minhhoang <boydammedamsamcotdien@gmail.com>
2025-11-19 23:06:21 +0700

    refactor: update main entry point and scripts in package.json
    - Changed main entry point from index.js to app.js
    - Added start and dev scripts for easier application management
    - Updated cookie dependency version
    - Downgraded express-device dependency version
    - Fixed package.json dependency version to avoid conflicts
    - Added nodemon as a dev dependency for development convenience

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
-var http = require('http');
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It works!\n',
-        version = 'NodeJS ' + process.versions.node + '\n',
-        response = [message, version].join('\n');
-    res.end(response);
+const express = require('express');
+const http = require('http');
+const cookieParser = require('cookie-parser');
+const cookie = require('cookie');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+
+// create http server using express
+const server = http.createServer(app);
+
+// const io = require('socket.io')(server);
+// const _chatSocket = require('./sockets/chat');
+// _chatSocket(io.of('/chat'));
+
+// Register middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('trust proxy', true)
+
+// API Endpoint
+_pageRouter = require('./routes/page');
+_pageRouter(app)
+
+_imagesRouter = require('./routes/images')
+_imagesRouter(app)
+
+_chessRouter = require('./routes/chess')
+_chessRouter(app)
+
+_AiRouter = require('./routes/AI');
+_AiRouter(app)
+
+
+// Start the server
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
-server.listen();
